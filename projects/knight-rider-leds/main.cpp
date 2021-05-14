52134ce96f16050e2c60ab1e78270124a89e405f
Ivan <capobianco.ivan@gmail.com>
2021-05-14 13:09:02 +0200

    style: comments

--- projects/knight-rider-leds/main.cpp
+++ projects/knight-rider-leds/main.cpp
@@ -2,13 +2,23 @@
 
 #define DELAY_MS 200
 
+/**
+ * DDRD, PORTD and PIND refer to pins D0 to D7
+ */
+
 void setup() {
+  // configuring the pins as output
   DDRD = 0xff;
+  // drive high all the pins 
   PORTD = 0xff;
 }
 
 void loop() {
   for (uint8_t i = 0; i < 8; i++) {
+    /**
+     * Writing a one to PIND toggles the value of PORTD
+     * indipendent on the value of DDRD
+     */
     PIND |= (1 << i);
     delay(DELAY_MS);
   }
